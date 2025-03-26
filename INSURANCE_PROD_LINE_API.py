2ffbcfb1f2c7ba0c0fdaa8629e71ff2671a3d331
sorawitr0607 <sorawit.r@tgh.co.th>
2025-03-26 16:46:20 +0700

    Update INSURANCE_PROD_LINE_API.py

--- INSURANCE_PROD_LINE_API.py
+++ INSURANCE_PROD_LINE_API.py
@@ -283,5 +283,4 @@ def handle_message(event):
     save_chat_history(user_id, "assistant", response, timestamp)
 
 if __name__ == "__main__":
-    app.run(debug=True,use_reloader=False)
-
+    app.run(host="0.0.0.0", port=8000)
