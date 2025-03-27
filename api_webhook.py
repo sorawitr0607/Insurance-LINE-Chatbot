79c88100a5bc1c6e9b144f1fcb1439e2075ba4d3
sorawitr0607 <sorawit.r@tgh.co.th>
2025-03-27 11:46:28 +0700

    Update api_webhook.py

--- api_webhook.py
+++ api_webhook.py
@@ -81,5 +81,4 @@ def handle_message(event):
     save_chat_history(user_id, "assistant", response, timestamp)
 
 if __name__ == "__main__":
-    #app.run(debug=True)
     app.run(host="0.0.0.0", port=8000)
